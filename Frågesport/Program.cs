e3150a2f2e5235daffdc3f68b665d02e93a97c4e
Andreas Thegelström <andreas.thegelstrom@elev.ga.ntig.sem>
2023-09-14 10:59:22 +0200

    Avklarade fråga 1

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int points =  0;
+
+Console.WriteLine("State your name challanger");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Well then " + name + " shall we get started?");
+Console.WriteLine( );
+Console.WriteLine("1st question: What game won the Game of the year awards year 2022? ");
+Console.WriteLine("A) God of war: Ragnarok. B) Elden ring. C) Stray. ");
+Console.ReadLine();
+string question1 = Console.ReadLine().ToLower();
+    
+    if (question1 == "B")
+    {
+        Console.WriteLine("Good job!");
+        points++;
+    }
+
+
+
+
+
+
+
+
+Console.ReadLine();
